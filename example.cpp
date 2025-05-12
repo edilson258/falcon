b9fdd594f3bc87c524ec041082c34e1214e2d8fe
edilson258 <dev.258.edilson@gmail.com>
2025-05-12 16:28:17 +0200

    docs(example): some improvments

--- example.cpp
+++ example.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <cstdlib>
+#include <iostream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "external/nlohmann/json.hpp"
@@ -11,53 +13,25 @@ public:
   int m_id;
   std::string m_email;
   std::string m_password;
-  bool m_is_deleted = false;
+  bool m_isdeleted = false;
 
-  user_schema(std::string email, std::string password) : m_email(email), m_password(password) {};
+  user_schema(std::string email, std::string pwd) : m_email(email), m_password(pwd) {};
 };
 
-std::vector<user_schema> users;
+std::vector<user_schema> users_db;
 
-fc::response create(fc::request &req) {
-  auto body = req.json();
-  user_schema user(body["email"], body["password"]);
-  users.push_back(user);
-  return fc::response::ok(fc::status::CREATED);
-}
-
-fc::response delet(fc::request &req) {
-  auto id = (std::stoi(std::string(req.get_param("id").value())) - 1);
-  if (id >= users.size() || users.at(id).m_is_deleted) {
-    return fc::response::ok(fc::status::NOT_FOUND);
-  }
-  users.at(id).m_is_deleted = true;
-  return fc::response::ok(fc::status::NO_CONTENT);
-}
-
-fc::response find_many(fc::request &req) {
-  nlohmann::json json = nlohmann::json::object();
-  for (user_schema &u : users) {
-    if (u.m_is_deleted) continue;
-    json["users"].push_back({{"email", u.m_email}, {"password", u.m_password}});
-  }
-  return fc::response::json(json);
-}
-
-fc::response find_by_id(fc::request &req) {
-  auto id = (std::stoi(std::string(req.get_param("id").value())) - 1);
-  if (id >= users.size() || users.at(id).m_is_deleted) {
-    return fc::response::ok(fc::status::NOT_FOUND);
-  }
-  return fc::response::json({{"email", users.at(id).m_email}, {"password", users.at(id).m_password}});
-}
+fc::response users_create(fc::request &req);
+fc::response users_delete(fc::request &req);
+fc::response users_find_many(fc::request &req);
+fc::response users_find_by_id(fc::request &req);
 
 fc::response auth_middleware(fc::request &);
 fc::response logger_middleware(fc::request &);
 
 int main(int argc, char *argv[]) {
   // mocked users
-  users.push_back(user_schema("alicey@email.com", "alice123"));
-  users.push_back(user_schema("milkey@test.com", "strongpass"));
+  users_db.push_back(user_schema("alicey@email.com", "alice123"));
+  users_db.push_back(user_schema("milkey@test.com", "strongpass"));
 
   fc::app app;
   app.set_views_dir("views/");
@@ -66,14 +40,15 @@ int main(int argc, char *argv[]) {
   fc::router router("/users");
 
   // middlewares
-  router.use(auth_middleware);
   router.use(logger_middleware);
+  router.use(auth_middleware);
 
-  router.post("", create);
-  router.get("", find_many);
-  router.get("/:id", find_by_id);
-  router.delet("/:id", delet);
+  router.post("", users_create);
+  router.get("", users_find_many);
+  router.get("/:id", users_find_by_id);
+  router.delet("/:id", users_delete);
 
+  // render html file
   app.get("/", [](fc::request &req) {
     return fc::response::render("index.html");
   });
@@ -82,21 +57,57 @@ int main(int argc, char *argv[]) {
   app.listen(":8000");
 }
 
+fc::response users_find_by_id(fc::request &req) {
+  auto id = (std::stoi(std::string(req.get_param("id").value())) - 1);
+  if (id >= users_db.size() || users_db.at(id).m_isdeleted) {
+    return fc::response::ok(fc::status::NOT_FOUND);
+  }
+  return fc::response::json({{"email", users_db.at(id).m_email}, {"password", users_db.at(id).m_password}});
+}
+
+fc::response users_find_many(fc::request &req) {
+  nlohmann::json json = nlohmann::json::object();
+  for (user_schema &u : users_db) {
+    if (u.m_isdeleted) continue;
+    json["users"].push_back({{"email", u.m_email}, {"password", u.m_password}});
+  }
+  return fc::response::json(json);
+}
+
+fc::response users_create(fc::request &req) {
+  auto body = req.json();
+  user_schema user(body["email"], body["password"]);
+  users_db.push_back(user);
+  return fc::response::ok(fc::status::CREATED);
+}
+
+fc::response users_delete(fc::request &req) {
+  auto id = (std::stoi(std::string(req.get_param("id").value())) - 1);
+  if (id >= users_db.size() || users_db.at(id).m_isdeleted) {
+    return fc::response::ok(fc::status::NOT_FOUND);
+  }
+  users_db.at(id).m_isdeleted = true;
+  return fc::response::ok(fc::status::NO_CONTENT);
+}
+
 fc::response logger_middleware(fc::request &req) {
-  // log something here
-  return req.next();
+  auto res = req.next();
+  std::cout << fc::method_to_string(req.get_method()) << " " << req.get_path() << " " << static_cast<int>(res.get_status()) << std::endl;
+  return res;
+}
+
+bool is_valid_token(std::string_view token) {
+  static std::string prefix = "Bearer ";
+  static std::string expected_token = "uGhTVjLwDb0R/s4xR3mwX/AdymqNbV9htkcRiulIw3E=";
+  if (!token.starts_with(prefix) || token.substr(prefix.length()) != expected_token) return false;
+  return true;
 }
 
 fc::response auth_middleware(fc::request &req) {
-  static std::string prefix = "Bearer ";
-  static std::string token = "uGhTVjLwDb0R/s4xR3mwX/AdymqNbV9htkcRiulIw3E=";
-  if (auto authToken = req.get_header("Authorization"); authToken.has_value()) {
-    if (!authToken.value().starts_with(prefix)) goto unauthorized;
-    if (authToken.value().substr(prefix.length()) != token) goto forbidden;
-    return req.next();
+  if (auto auth_header = req.get_header("Authorization"); auth_header.has_value()) {
+    if (is_valid_token(auth_header.value())) {
+      return req.next();
+    }
   }
-unauthorized:
   return fc::response::ok(fc::status::UNAUTHORIZED);
-forbidden:
-  return fc::response::ok(fc::status::FORBIDDEN);
 }
